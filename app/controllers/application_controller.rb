c4ee7a8cb2e4061714e5b099f8a851a2752a218b
Justin Walker <jrwalker314@yahoo.com>
2017-09-19 23:57:10 +0000

    Add application_controller pundit setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
   before_action :authenticate_user!
+  
+  private 
+  def user_not_authorized
+    flash[:alert] = "Access Denied"
+    redirect_to (request.referrer || root_path)
+  end
 end
